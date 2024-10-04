da6eee8efee23dbf16c36cb990622106827b12c0
jemimakathar <katharolijemima@gmail.com>
2024-10-04 17:54:48 +0530

    uploaded file

--- challenges/right_angle.js
+++ challenges/right_angle.js
@@ -1,10 +1,12 @@
+"use strict";
 // Input: 5
 // Output:
-// *
-// **
-// ***
-// ****
-// *****
+// 1
+// 22
+// 333
+// 4444
+// 55555
+Object.defineProperty(exports, "__esModule", { value: true });
 function printRightAngledTriangle(rows) {
     for (var i = 1; i <= rows; i++) {
         var number = '';
@@ -16,3 +18,27 @@ function printRightAngledTriangle(rows) {
 }
 var numberOfRows = 5;
 printRightAngledTriangle(numberOfRows);
+console.log("------------------------------------");
+// 1
+// 2 3
+// 4 5 6
+// 7 8 9 10
+function printRightAngledTriangles(rows) {
+    var num = 1;
+    for (var i = 1; i <= rows; i++) {
+        var number = '';
+        for (var j = 1; j <= i; j++) {
+            number += " ".concat(num);
+            num++;
+        }
+        console.log(number.trim());
+    }
+}
+var numberOfRow = 5;
+printRightAngledTriangles(numberOfRow);
+console.log("--------------------------------");
+//      1
+//     2 2
+//    3 3 3
+//   4 4 4 4
+//  5 5 5 5 5
--- challenges/right_angle.ts
+++ challenges/right_angle.ts
@@ -6,6 +6,7 @@
 // 4444
 // 55555
 
+import { log } from "console";
 
 function printRightAngledTriangle(rows: number):void{
     for (let i = 1; i <= rows; i++) {
@@ -18,3 +19,61 @@ function printRightAngledTriangle(rows: number):void{
 }
 const numberOfRows = 5;
 printRightAngledTriangle(numberOfRows);
+console.log("------------------------------------")
+
+
+
+// 1
+// 2 3
+// 4 5 6
+// 7 8 9 10
+
+function printRightAngledTriangles(rows: number):void{
+    let num=1;
+    for (let i = 1; i <= rows; i++) {
+       
+        let number = '';
+        for (let j = 1; j <= i;j++) {
+            number += ` ${num}`;
+            num++; 
+        }
+        console.log(number.trim());
+    }
+}
+const numberOfRow = 5;
+printRightAngledTriangles(numberOfRow);
+console.log("--------------------------------")
+
+
+
+//      1
+//     2 2
+//    3 3 3
+//   4 4 4 4
+//  5 5 5 5 5
+
+
+function triangles(rows: number):void{
+    let num=1;
+    for (let i = 1; i <= rows; i++) {
+       
+        let number = '';
+        for (let j = 1; j <= i;j++) {
+            number += ` ${num}`;
+            num++; 
+        }
+        console.log(number.trim());
+    }
+}
+const numberRow = 5;
+triangles(numberRow);
+console.log("--------------------------------")
+
+
+
+
+
+
+
+
+
