5e79c3a91e51361cbfd60117423555a955301cfe
jemimakathar <katharolijemima@gmail.com>
2024-10-08 18:12:32 +0530

    uploaded file

---
+++ challenges/starting_alphabet.ts
@@ -0,0 +1,7 @@
+// Write a function that takes an array of strings as argument. Group those strings by their first letter.
+//  Return an object that contains properties with keys representing first letters.
+//   The values should be arrays of strings containing only the corresponding strings.
+
+
+//    For example, the array ['Alf', 'Alice', 'Ben'] should be transformed to
+//    { a: ['Alf', 'Alice'], b: ['Ben']}
