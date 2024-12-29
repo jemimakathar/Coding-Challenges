4b802783d1bc1c42f2c5af082169035338ded758
jemimakathar <katharolijemima@gmail.com>
2024-12-29 19:28:30 +0530

    uploaded

--- challenges/consestent.js
+++ challenges/consestent.js
@@ -1,9 +1,6 @@
 // 1684. Count the Number of Consistent Strings
 // You are given a string allowed consisting of distinct characters and an array of strings words. A string is consistent if all characters in the string appear in the string allowed.
 // Return the number of consistent strings in the array words.
-
-
- 
 // Example 1:
 // Input: allowed = "ab", words = ["ad","bd","aaab","baa","badab"]
 // Output: 2
@@ -16,29 +13,21 @@
 // Input: allowed = "cad", words = ["cc","acd","b","ba","bac","bad","ac","d"]
 // Output: 4
 // Explanation: Strings "cc", "acd", "ac", and "d" are consistent.
-
-function countConsistentStrings(words:string[],allow:string)
-{
-    let count=0
-    for(var i=0;i<words.length;i++)
-    {
-        var consist:boolean=true;
-        for(var j=0;j<words[i].length;j++)
-        {
-            if(!allow.includes(words[i][j]))
-            {
+function countConsistentStrings(words, allow) {
+    var count = 0;
+    for (var i = 0; i < words.length; i++) {
+        var consist = true;
+        for (var j = 0; j < words[i].length; j++) {
+            if (!allow.includes(words[i][j])) {
                 var consist = false;
                 break;
             }
         }
-        if(consist)
-        {
+        if (consist) {
             count++;
-        }  
+        }
     }
     return count;
 }
-const result = countConsistentStrings(["cc","acd","b","ba","bac","bad","ac","d"], "cad");
-console.log(result); 
-
- 
+var result = countConsistentStrings(["cc", "acd", "b", "ba", "bac", "bad", "ac", "d"], "cad");
+console.log(result); // Output: 4
